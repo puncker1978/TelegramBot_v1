215e366a4c34931f989084482c09b74594a7df3c
Puncker1978 <nvv197827@gmail.com>
2022-07-31 20:05:03 +0500

    Добавьте файлы проекта.

---
+++ TelegramBot_v1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot_v1
+{
+    class Program
+    {
+        static TelegramBotClient bot;
+
+        [Obsolete]
+        static void Main(string[] args)
+        {
+
+            string token = "TOKEN";
+
+            bot = new TelegramBotClient(token);
+            //bot.OnMessage += MessageListener;
+            bot.OnMessage += MessageListener;
+            bot.StartReceiving();
+            Console.ReadKey();
+        }
+
+        [Obsolete]
+        private static void MessageListener(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            string text = $"{DateTime.Now.ToLongTimeString()}: {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text}";
+
+            Console.WriteLine($"{text} TypeMessage: {e.Message.Type}");
+
+
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Document)
+            {
+                Console.WriteLine(e.Message.Document.FileId);
+                Console.WriteLine(e.Message.Document.FileName);
+                Console.WriteLine(e.Message.Document.FileSize);
+
+                DownLoad(e.Message.Document.FileId, e.Message.Document.FileName);
+            }
+
+            if (e.Message.Text == null) return;
+
+            var messageText = e.Message.Text;
+
+
+            bot.SendTextMessageAsync(e.Message.Chat.Id, $"{messageText}");
+        }
+
+        static async void DownLoad(string fileId, string path)
+        {
+            var file = await bot.GetFileAsync(fileId);
+            FileStream fs = new FileStream("_" + path, FileMode.Create);
+            await bot.DownloadFileAsync(file.FilePath, fs);
+            fs.Close();
+
+            fs.Dispose();
+        }
+    }
+}
